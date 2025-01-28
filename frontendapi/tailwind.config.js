003ce50c4f31be3335e8eb57733f5571c090d40c
Dario Restrepo Landazury <drestrepo@unipacifico.edu.co>
2025-01-28 16:34:50 -0500

    Añadir configuración inicial de Vite con soporte para React y TypeScript, incluyendo archivos de configuración, estilos y estructura básica de la aplicación.

---
+++ frontendapi/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // Ajusta si cambian las rutas de tus archivos
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
